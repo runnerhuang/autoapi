862caa8c8209c32dfe525c6f169aac0abd865c63
runnerhuang <45131712@qq.com>
2020-03-13 18:49:08 +0800

    delete some useless information

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ ap = ArgumentParser(
 
 ap.add_argument('-F', dest='projectCaseFolder', action='store', type=str, nargs='?',
                 default='TestCase//BAIDU//LOGIN//LOGIN')
-# default='TestCase//EATOJOY//ALL//ALL')
 ap.add_argument('-N', dest='number', action="store", type=int, nargs='?',
                 default='002')  # single case number，if 0 then all
 ap.add_argument('-S', dest='sleep_time', action="store", type=float, nargs='?', default=0)
